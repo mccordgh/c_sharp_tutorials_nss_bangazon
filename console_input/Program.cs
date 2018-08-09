205f0cafc7e8c67a4e35967c634b6a50d26531de
Matthew McCord <mattm.eternal@gmail.com>
2018-08-09 10:59:07 -0500

    Console Input tutorial

---
+++ console_input/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace console_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nWhat is your name, dummy? ");
+            string name = Console.ReadLine();
+
+            Banner($"Greetings, {name}!!");
+
+            Boolean correctKey = false;
+
+            do {
+                Console.WriteLine($"Now, do me a favor and press the [K] key would ya?");
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                correctKey = key.Key == ConsoleKey.K;
+
+                if (!correctKey) {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Uhm, I said the [K] key ya dingus...");
+                    Console.WriteLine();
+                }
+            } while (!correctKey);
+
+            Banner($"YAY YOU CAN READ!!!");
+        }
+
+        private static void Banner(string text) {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("************************************");
+            Console.WriteLine($"\t{text}");
+            Console.WriteLine("************************************");
+            Console.WriteLine();
+        }
+    }
+}
