ea804738f668ddcee1383b09c501cc48a088c42c
Matthew McCord <mattm.eternal@gmail.com>
2018-08-09 10:59:15 -0500

    Intro tutorial

---
+++ intro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace bangazon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime purchaseDate = DateTime.Now;
+
+            string lastName = "Smith";
+
+            var firstName = "Bill";
+
+            Console.WriteLine($"\n{firstName} {lastName} purchased on {purchaseDate}\n");
+
+            string[] products = new string[] {
+                "Motorcycle",
+                "Sofa",
+                "Sandals",
+                "Omega Watch",
+                "iPhone"
+            };
+
+            foreach (string product in products) {
+                if (product.Length > 6) {
+                    Console.WriteLine($"{product} *");
+                } else {
+                    Console.WriteLine(product);
+                }
+            }
+        }
+    }
+}
